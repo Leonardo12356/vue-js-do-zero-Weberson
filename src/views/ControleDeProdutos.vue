699f10144a0d4a8067422bd934ad94ec72f46be5
Leonardo <leonardoaguiar1235@gmail.com>
2022-09-11 15:53:55 -0300

    Componente menu e rotas

---
+++ src/views/ControleDeProdutos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div> <!--Dentro de uma template só pode ter um filho com tudo dentro dela, ex: div que foi minha tag principal-->
+    <h1>{{ mensagem }}</h1> <!--posso chamar qualquer coisa dentro do data INTERPOLAÇÃO -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ControleDeProdutos',
+  data(){
+    return{
+      mensagem: 'Estou na tela Controle de produtos' // posso usar em qualquer lugar é como se fosse uma variável 
+    }
+  }
+}
+</script>
+
+<!--CSS não fica preso à um componente somente com a palavra 'SCOPED'-->
+
+<style scoped> 
+h1 {
+  color: red;
+}
+
+</style>
